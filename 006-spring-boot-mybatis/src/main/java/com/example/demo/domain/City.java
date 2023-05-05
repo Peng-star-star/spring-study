7e6046267e8e64b06a5b44fe804a4e039a34898a
pengl <961159376@qq.com>
2023-05-05 12:57:19 +0800

    add spring boot 集成 mybastis

---
+++ 006-spring-boot-mybatis/src/main/java/com/example/demo/domain/City.java
@@ -0,0 +1,12 @@
+package com.example.demo.domain;
+
+import lombok.Data;
+
+@Data
+public class City {
+
+    private Long id;
+    private String name;
+    private String state;
+    private String country;
+}
